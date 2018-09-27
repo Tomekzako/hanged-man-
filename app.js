63de8e1cfef57b62a9e0f5d2bb28e744b09a45c4
Tomasz Chyc <tomek.chyc55@gmail.com>
2018-09-27 15:48:18 +0200

    create alphabet

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function() {
+  const password = "Bez pracy nie ma kołaczy".toUpperCase();
+  let hiddenPassword = "";
 
-    const password = "Bez pracy nie ma kołaczy".toUpperCase();
-    let hiddenPassword = "";
-
-    for (el in password) {
-        if (password.charAt(el) == " ") {
-            hiddenPassword += " ";
-        } else {
-            hiddenPassword += "-";
-
-        }
+  for (el in password) {
+    if (password.charAt(el) == " ") {
+      hiddenPassword += " ";
+    } else {
+      hiddenPassword += "-";
     }
+  }
 
-    console.log(hiddenPassword);
+  console.log(hiddenPassword);
 
-    function writePassword() {
-        document.querySelector('.board').innerHTML = hiddenPassword;
-    }
+  function writePassword() {
+    document.querySelector(".board").innerHTML = hiddenPassword;
+  }
 
+  function init() {
+    let letter = "";
+    const alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzżź"
+      .toUpperCase()
+      .split("");
+
+    const div = alphabet
+      .map(el => {
+        return `<div class="letter">${el}</div>`;
+      })
+      .join("");
+
+    document.querySelector(".alphabet").innerHTML = div;
     writePassword();
+  }
 
+  init();
 });
